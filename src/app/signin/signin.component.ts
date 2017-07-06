3b884d07c7f5e52ed3db658291e9fd956a27c8f7
Tafarel Mello <tafarel.mello@gmail.com>
2017-07-06 10:16:52 -0300

    First commit

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit {
+  form: FormGroup;
+  error = false;
+  errorMessage = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  onSignin() {
+    this.authService.signIn(this.form.value);
+  }
+
+  ngOnInit(): any {
+    this.form = this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+  }
+}
